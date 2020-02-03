7112c739ac35ab7793590bcc130ed22da84d7ff6
James Leahy <defuncart@gmail.com>
2020-02-03 18:46:08 +0100

    Update test filenames.

--- test/flutter_pseudolocalizor_test.dart
+++
@@ -1,9 +0,0 @@
-import 'models/models.dart' as models_test;
-import 'services/csv_generator.dart' as csvGenerator_test;
-import 'utils/utils.dart' as utils_test;
-
-void main() {
-  models_test.main();
-  csvGenerator_test.main();
-  utils_test.main();
-}
