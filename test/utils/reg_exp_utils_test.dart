679391b8045f20e50c44add9e6077c0fd8d6d5c2
James Leahy <defuncart@gmail.com>
2020-06-13 09:39:01 +0200

    Add tests for RegExpUtils.

---
+++ test/utils/reg_exp_utils_test.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_pseudolocalizor/src/utils/reg_exp_utils.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('combinePatterns: patterns is null, expect null RegExp', () {
+    final regExp = RegExpUtils.combinePatterns(null);
+    expect(regExp, isNull);
+  });
+
+  test('combinePatterns: patterns is empty, expect null RegExp', () {
+    final regExp = RegExpUtils.combinePatterns([]);
+    expect(regExp, isNull);
+  });
+
+  test('combinePatterns: Valid patterns, valid regexp', () {
+    final regExp = RegExpUtils.combinePatterns(['%(\S*?)\$[ds]', 'Flutter']);
+    expect(regExp, isNotNull);
+    expect(regExp.pattern, '%(\S*?)\$[ds]|Flutter');
+  });
+}
