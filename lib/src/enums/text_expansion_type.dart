f9f33f9fd2a0cefdc6e826f650c49f141fe50081
James Leahy <defuncart@gmail.com>
2021-05-21 15:19:57 +0200

    Add TextExpansionType

---
+++ lib/src/enums/text_expansion_type.dart
@@ -0,0 +1,8 @@
+/// An enum which describes the different text expansion options
+enum TextExpansionType {
+  /// append text at the end of the string, i.e. MyString abd
+  append,
+
+  /// repeats vowels, i.e. MyyStriing
+  repeatVowels,
+}
