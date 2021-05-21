fb889043e37ce976276a62273c88f67c309eb2d8
James Leahy <defuncart@gmail.com>
2021-05-21 15:18:12 +0200

    Add string extensions.

---
+++ test/extensions/string_extensions_test.dart
@@ -0,0 +1,59 @@
+import 'package:flutter_pseudolocalizor/src/extensions/string_extensions.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('StringExtensions', () {
+    group('isVowel', () {
+      test('When a, e, i, o, u, y, expect true', () {
+        final letters = ['a', 'e', 'i', 'o', 'u', 'y'];
+
+        for (final letter in letters) {
+          expect(letter.isVowel, isTrue);
+          expect(letter.toUpperCase().isVowel, isTrue);
+        }
+      });
+
+      test('When b, c, d etc, expect false', () {
+        final letters = [
+          'b',
+          'c',
+          'd',
+          'f',
+          'g',
+          'h',
+          'j',
+          'k',
+          'l',
+          'm',
+          'n',
+          'p',
+          'q',
+          'r',
+          's',
+          't',
+          'v',
+          'w',
+          'x',
+          'z',
+        ];
+
+        for (final letter in letters) {
+          expect(letter.isVowel, isFalse);
+          expect(letter.toUpperCase().isVowel, isFalse);
+        }
+      });
+
+      test('when not a single character, expect assertion', () {
+        expect(
+          () => ''.isVowel,
+          throwsArgumentError,
+        );
+
+        expect(
+          () => 'aa'.isVowel,
+          throwsArgumentError,
+        );
+      });
+    });
+  });
+}
