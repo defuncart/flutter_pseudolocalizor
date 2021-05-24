7b07988d1c6a13b01564490c2540d5d34e63fb20
James Leahy <defuncart@gmail.com>
2021-05-24 07:54:28 +0200

    CSVSettings improve documentation

--- lib/src/models/csv_settings.dart
+++ lib/src/models/csv_settings.dart
@@ -2,7 +2,7 @@ import '../configs/csv_default_settings.dart';
 
 /// A model representing CSV File parsing settings
 class CSVSettings {
-  /// A filepath for the output file. Defaults to `<input_filename>_PSEUDO.<extension>`.
+  /// An optional filepath for the output file.
   final String? outputFilepath;
 
   /// The delimiter to use. Defaults to `,`.
@@ -13,9 +13,8 @@ class CSVSettings {
 
   /// Constructs a new instance of [CSVSettings]
   ///
-  /// [outputFilepath] is required. If null, defaults to `CSVDefaultSettings.delimiter`.
-  /// [delimiter] is required. If null, defaults to `CSVDefaultSettings.delimiter`.
-  /// [columnIndex] is required. If null, defaults to `CSVDefaultSettings.columnIndex`.
+  /// When null, [delimiter] defaults to `CSVDefaultSettings.delimiter`.
+  /// When null, [columnIndex] defaults to `CSVDefaultSettings.columnIndex`.
   CSVSettings({
     this.outputFilepath,
     String? delimiter,
