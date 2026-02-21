171015de356c02d40b1c24e918528ec3a9c67e55
James Leahy <defuncart@gmail.com>
2026-02-21 20:07:12 +0100

    Dart 3 code changes

--- lib/src/extensions/unicode_block_extensions.dart
+++ lib/src/extensions/unicode_block_extensions.dart
@@ -7,34 +7,22 @@ import '../enums/unicode_block.dart';
 
 extension UnicodeBlockExtensions on UnicodeBlock {
   /// An array of special characters
-  List<String> get specialCharacters {
-    switch (this) {
-      case UnicodeBlock.latinSupplement:
-        return LatinSupplement.specialCharacters;
-      case UnicodeBlock.latinExtendedA:
-        return LatinExtendedA.specialCharacters;
-      case UnicodeBlock.latinExtendedB:
-        return LatinExtendedB.specialCharacters;
-      case UnicodeBlock.latinExtendedC:
-        return LatinExtendedC.specialCharacters;
-      case UnicodeBlock.superscriptsSubscripts:
-        return SuperscriptsSubscripts.specialCharacters;
-    }
-  }
+  List<String> get specialCharacters => switch (this) {
+        UnicodeBlock.latinSupplement => LatinSupplement.specialCharacters,
+        UnicodeBlock.latinExtendedA => LatinExtendedA.specialCharacters,
+        UnicodeBlock.latinExtendedB => LatinExtendedB.specialCharacters,
+        UnicodeBlock.latinExtendedC => LatinExtendedC.specialCharacters,
+        UnicodeBlock.superscriptsSubscripts =>
+          SuperscriptsSubscripts.specialCharacters,
+      };
 
   /// A dictionary of mapping characters
-  Map<String, List<String>> get mappingCharacters {
-    switch (this) {
-      case UnicodeBlock.latinSupplement:
-        return LatinSupplement.mappingCharacters;
-      case UnicodeBlock.latinExtendedA:
-        return LatinExtendedA.mappingCharacters;
-      case UnicodeBlock.latinExtendedB:
-        return LatinExtendedB.mappingCharacters;
-      case UnicodeBlock.latinExtendedC:
-        return LatinExtendedC.mappingCharacters;
-      case UnicodeBlock.superscriptsSubscripts:
-        return SuperscriptsSubscripts.mappingCharacters;
-    }
-  }
+  Map<String, List<String>> get mappingCharacters => switch (this) {
+        UnicodeBlock.latinSupplement => LatinSupplement.mappingCharacters,
+        UnicodeBlock.latinExtendedA => LatinExtendedA.mappingCharacters,
+        UnicodeBlock.latinExtendedB => LatinExtendedB.mappingCharacters,
+        UnicodeBlock.latinExtendedC => LatinExtendedC.mappingCharacters,
+        UnicodeBlock.superscriptsSubscripts =>
+          SuperscriptsSubscripts.mappingCharacters,
+      };
 }
