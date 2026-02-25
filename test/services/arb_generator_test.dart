ee3a8302652ac0c374bd16d067741536321ac84d
James Leahy <defuncart@gmail.com>
2026-02-25 09:22:33 +0100

    Improve arb tests

--- test/services/arb_generator_test.dart
+++ test/services/arb_generator_test.dart
@@ -2,11 +2,38 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter_pseudolocalizor/flutter_pseudolocalizor.dart';
+import 'package:flutter_pseudolocalizor/src/enums/supported_language.dart';
 import 'package:flutter_pseudolocalizor/src/services/arb_generator.dart';
 import 'package:test/test.dart';
 
 void main() {
   final fileContents = '''
+{
+  "@@locale": "en",
+  "myKey": "Hello world!",
+  "@myKey": {},
+  "welcome": "Welcome {firstName}!",
+  "@welcome": {},
+  "numberMessages": "{count, plural, zero{You have no new messages} one{You have 1 new message} other{You have {count} new messages}}",
+  "@numberMessages": {
+    "description": "An info message about new messages count"
+  },
+  "whoseBook": "{sex, select, male{His book} female{Her book} other{Their book}}",
+  "@whoseBook": {
+    "description": "A message determine whose book it is"
+  },
+  "unreadEmails": "{howMany, plural, zero{There are no unread emails for {userName}} one{There is 1 unread email for {userName}} other{There are {howMany} unread emails for {userName}}}",
+  "@unreadEmails": {
+    "description": "How many unread emails for user"
+  },
+  "weatherReaction": "{weatherType, select, sunny{Woohoo} cloudy{Meh} rainy{Weeh} other{Other}}",
+  "@weatherReaction": {
+    "description": "Reaction to types of weather"
+  }
+}
+''';
+
+  final fileContentsShort = '''
 {
   "@@locale": "en",
   "myKey": "Hello world!",
@@ -38,12 +65,29 @@ void main() {
 
     final contents = ARBGenerator.generate(file, packageSettings);
     expect(contents, isNotNull);
+
     final decodedContents = jsonDecode(contents);
-    expect(decodedContents['@@locale'], 'en');
-    expect(decodedContents['myKey'], isNotNull);
-    expect(decodedContents['myKey'], isNot('Hello world!'));
-    expect(decodedContents['welcome'], isNotNull);
-    expect(decodedContents['welcome'], isNot('Welcome {firstName}!'));
+    expect(
+      decodedContents,
+      {
+        '@@locale': 'en',
+        'myKey': '[Ȟₑɇɇéⱹⱡⱺǫ ⱳðȍɍĺǆ¡]',
+        '@myKey': {},
+        'welcome': '[Ɯǝɇëēȇēⱸêłȼȯöȫôȣȭmȩę {firstName}¡]',
+        '@welcome': {},
+        'numberMessages':
+            '{count, plural, zero{[¥ðǭŭµ ĥaaⱱėė ñðƣ ŋǝēŵ mĕȩśƨaaĝěƨ]} one{[Ƴºōùű ⱨaaⱽēǝ ¹ ȵēěŵ mȇǝƽȿaaǵĕ]} other{[Ȳœȍðȍűùǔ ƕaⱽɇ {count} ⁿêęₑȇŵ mĕⱸₔęșšaaağₔǝș]}}',
+        '@numberMessages': {'description': 'An info message about new messages count'},
+        'whoseBook': '{sex, select, male{[Ħïǁìȿ ƃõƣōǫⱪ]} female{[Ȟȩēêɍ ƅơȫȯȫķ]} other{[Ƭȟęęⱻǁǃɍ ƀơôŏðĸ]}}',
+        '@whoseBook': {'description': 'A message determine whose book it is'},
+        'unreadEmails':
+            '{howMany, plural, zero{[Ʈħɇèęéɍȇēⱻè aaaa®ėₔēě ⁿₒǒȍó ŭůǚǜňŕⱸēaaď ȅëmaîǉș ſö® {userName}]} one{[Țĥƹéěȩřȩⱻĕȩ ǁìįⁱś ₁ ǔµưŭǹřȅⱻȩēaaaƌ ĕƹmaaȉƪ ſōŗ {userName}]} other{[Ʈƕȇĕèǝ®ⱸĕƹȇ aaŕè {howMany} ưüǘǚƞŕȩȅₑëaaaaƌ ƹⱻëmaaǁƪȿ ſǫȓ {userName}]}}',
+        '@unreadEmails': {'description': 'How many unread emails for user'},
+        'weatherReaction':
+            '{weatherType, select, sunny{[Ⱳⱷơº°ⱨºŏǒ]} cloudy{[Ɱₑēⱸⱨ]} rainy{[Ⱳèëĕėȟ]} other{[ŌǾȪⱦⱨêëř]}}',
+        '@weatherReaction': {'description': 'Reaction to types of weather'}
+      },
+    );
 
     // clear up and delete file
     file.deleteSync();
@@ -52,7 +96,7 @@ void main() {
   test('languagesToGenerate', () {
     // create file
     final file = File('_.arb');
-    file.writeAsStringSync(fileContents);
+    file.writeAsStringSync(fileContentsShort);
 
     // initialize settings
     final packageSettings = PackageSettings(
@@ -69,6 +113,30 @@ void main() {
       keysToIgnore: null,
     );
 
+    final expectedValues = {
+      SupportedLanguage.de: {
+        "@@locale": "de",
+        "myKey": "[Heeeellöö wöörld!]",
+        "@myKey": {},
+        "welcome": "[Weeeeeeeelcöööööömee {firstName}!]",
+        "@welcome": {}
+      },
+      SupportedLanguage.es: {
+        "@@locale": "es",
+        "myKey": "[Hééééllóó wóórld!]",
+        "@myKey": {},
+        "welcome": "[Wéééééééélcóóóóóóméé {firstName}!]",
+        "@welcome": {}
+      },
+      SupportedLanguage.pl: {
+        "@@locale": "pl",
+        "myKey": "[Hęęęęłłóó wóórłd!]",
+        "@myKey": {},
+        "welcome": "[Węęęęęęęęłćóóóóóómęę {firstName}!]",
+        "@welcome": {}
+      }
+    };
+
     for (final language in packageSettings.languagesToGenerate!) {
       final contents = ARBGenerator.generate(
         file,
@@ -76,13 +144,9 @@ void main() {
         supportedLanguage: language,
       );
       expect(contents, isNotNull);
+
       final decodedContents = jsonDecode(contents);
-      expect(decodedContents['@@locale'], isNot('en'));
-      expect(decodedContents['@@locale'], language.name);
-      expect(decodedContents['myKey'], isNotNull);
-      expect(decodedContents['myKey'], isNot('Hello world!'));
-      expect(decodedContents['welcome'], isNotNull);
-      expect(decodedContents['welcome'], isNot('Welcome {firstName}!'));
+      expect(decodedContents, expectedValues[language]);
     }
 
     // clear up and delete file
